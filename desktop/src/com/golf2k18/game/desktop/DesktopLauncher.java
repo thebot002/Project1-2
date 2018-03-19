e2f756f128bd33fb1000ecb60056ae2d927b63fa
thebot002 <31898693+thebot002@users.noreply.github.com>
2018-03-19 13:48:40 +0100

    Initial commit

---
+++ desktop/src/com/golf2k18/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.golf2k18.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.golf2k18.game.golf_2k18;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new golf_2k18(), config);
+	}
+}
