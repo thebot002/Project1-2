14af4cee649b8ee1148a3caa5a4f12db2358e6e9
snadiraps <snadiraps@users.noreply.github.com>
2018-03-20 13:05:08 +0100

    Merge branch 'States' of C:\Users\Luc\IdeaProjects\Project1.2 with conflicts.

--- core/src/com/golf2k18/GameStates/Menu.java
+++ core/src/com/golf2k18/GameStates/Menu.java
@@ -1,12 +1,15 @@
 package com.golf2k18.GameStates;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Menu extends GameState
 {
+	private Texture backgroundMenu;
 	public Menu(GameStateManager gsm)
 	{
 		super(gsm);
+		backgroundMenu = new Texture("MiniGolf WIndmill.jpg");
 	}
 	public void handleInput()
 	{
@@ -16,8 +19,9 @@ public class Menu extends GameState
 	{
 		
 	}
-	public void render(SpriteBatch s)
+	public void render(SpriteBatch sb)
 	{
-		
+		sb.begin();
+		sb.draw(backgroundMenu, 0, 0, golf2k18.HEIGHT, golf2k18.WIDTH);
 	}
 }
--- core/src/com/golf2k18/game/golf_2k18.java
+++ core/src/com/golf2k18/game/golf_2k18.java
@@ -3,31 +3,34 @@ package com.golf2k18.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.golf2k18.GameStates.GameStateManager;
 
-public class golf_2k18 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class golf_2k18 extends ApplicationAdapter 
+{
+	private SpriteBatch batch;
+	private GameStateManager gsm;
+
+	public static final String title = "Golf2k18";
 	@Override
-	public void create () {
+	public void create () 
+	{
+		gsm = new GameStateManager();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render ()
+	{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose () 
+	{
 		batch.dispose();
-		img.dispose();
 	}
 }
