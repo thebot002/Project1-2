bca7ff360fc4e8ea3ea74fe48c30d203216a7a1d
snadiraps <snadiraps@users.noreply.github.com>
2018-03-20 10:58:00 +0100

    I forgot some things...

--- core/src/com/golf2k18/GameStates/GameStateManager.java
+++ core/src/com/golf2k18/GameStates/GameStateManager.java
@@ -2,10 +2,12 @@ package com.golf2k18.GameStates;
 
 import java.util.Stack;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class GameStateManager
 {
 	private Stack<GameState> states;
-	
+
 	public GameStateManager()
 	{
 		states = new Stack<GameState>();
@@ -17,5 +19,18 @@ public class GameStateManager
 	public void pop()
 	{
 		states.pop();
+	}
+	public void set(GameState state)
+	{
+		states.pop();
+		states.push(state);
+	}
+	public void update(float dt)
+	{
+		states.peek().update(dt);
+	}
+	public void render(SpriteBatch sb)
+	{
+		states.peek().render(sb);
 	}
 }
