27b1a3d3927008806ecde948d8c168c968792af3
snadiraps <snadiraps@users.noreply.github.com>
2018-03-21 16:45:28 +0100

    Import/Export state update

--- core/src/com/golf2k18/GameStates/IOMenu.java
+++ core/src/com/golf2k18/GameStates/IOMenu.java
@@ -4,12 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class IOMenu extends GameState
 {
@@ -22,9 +21,13 @@ public class IOMenu extends GameState
         super(gsm);
         skin = new Skin(Gdx.files.internal("Skins/gdx-skins-master/flat-earth/skin/flat-earth-ui.json"));
         this.batch = batch;
-        createStage();
         saves = new Array<String>();
         this.gsm = gsm;
+       //temp
+        saves.add("Under");
+        saves.add("Construction");
+        //temp
+        createStage();
     }
 
     private void createStage()
@@ -32,6 +35,36 @@ public class IOMenu extends GameState
         stage = new Stage(new ScreenViewport(),batch);
         Table table = new Table();
         table.setFillParent(true);
+        //Title
+        Label ioMenu = new Label("IO-MENU", skin, "title");
+        table.add(ioMenu).center().pad(100f);
+        table.row();
+
+        //ImportLabel
+        Label importLabel = new Label("Enter path for import file:", skin);
+        table.add(importLabel).center().padTop(10f);
+        table.row();
+
+        //ImportTextField
+        TextField path = new TextField("C:", skin);
+        table.add(path).center().fillX().pad(10f);
+        table.row();
+
+        //ImportButton
+        TextButton importBtn = new TextButton("Import", skin);
+        table.add(importBtn).center().pad(10f);
+        table.row();
+
+        //Export table
+        List<String> list = new List<String>(skin);
+        list.setItems(saves);
+        table.add(list).center().fillX().pad(10f);
+        table.row();
+
+        //ExportButton
+        TextButton exportBtn = new TextButton("Export", skin);
+        table.add(exportBtn).center().pad(10f);
+        table.row();
 
         //Return button
         TextButton returnBtn = new TextButton("Return", skin);
