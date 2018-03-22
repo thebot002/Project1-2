177a7a3416850b1b33df74e214ebba85db560bcc
snadiraps <snadiraps@users.noreply.github.com>
2018-03-22 13:03:10 +0100

    Course update, engine

--- core/src/com/golf2k18/objects/Course.java
+++ core/src/com/golf2k18/objects/Course.java
@@ -16,7 +16,7 @@ public class Course {
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = new Point(0,Math.random()*2); //random heights & friction value //The height will be determined by solving the equation
+                grid[i][j] = new Point(Math.cos(i),Math.random()*2); //random heights & friction value //The height will be determined by solving the equation
             }
         }
     }
---
+++ core/src/com/golf2k18/objects/Engine.java
@@ -0,0 +1,15 @@
+package com.golf2k18.objects;
+
+public class Engine
+{
+    private Vector acceleration;
+    private final double GRAVITY = 9.81;
+    public Engine()
+    {
+
+    }
+    public Vector calcGravity(Ball ball)
+    {
+
+    }
+}
