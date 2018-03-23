f0358016528d05c16b2e0e2739bf1b3082c02dad
snadiraps <snadiraps@users.noreply.github.com>
2018-03-23 12:10:20 +0100

    Import module

--- core/src/com/golf2k18/objects/Course.java
+++ core/src/com/golf2k18/objects/Course.java
@@ -4,7 +4,7 @@ public class Course {
     public int courseWidth;
     public int courseHeight;
     public String[] formula;
-    public final double MU = 0.5;
+    public double MU = 0.5;
     public double[] start = {0,0,0};
     public double[] goal = {0.0,1.0,0.0};
     public double tolerance = 0.02;
