64244d8c6e1b53079c6e33b8973c19e9f3653f70
Andreas <andreaszurhaar@gmail.com>
2018-05-28 15:23:28 +0200

    updated AI

--- core/src/com/golf2k18/AI/AI.java
+++ core/src/com/golf2k18/AI/AI.java
@@ -1,32 +1,39 @@
+
 package com.golf2k18.AI;
 
 import com.badlogic.gdx.math.Vector3;
 import com.golf2k18.objects.Ball;
+import com.golf2k18.objects.Goal;
 import com.golf2k18.objects.Terrain;
 
 public class AI {
     private Ball ball;
     private Terrain terrain;
+    private Goal goal;
     public void AI(Ball ball, Terrain terrain){
         this.ball = ball;
         this.terrain = terrain;
+        this.goal = terrain.getGoal();
+
     }
     public Vector3 holeInOne(){
         Vector3 addVectors = new Vector3();
-        while(!terrain.isGoalHit())
+
+        while(!goal.isHit(ball))
         {
             Vector3 tryBall = fitness();
-            addVectors = addVectors.add(tryBall);
-            ball.hit(addVectors);
+
         }
         return addVectors;
     }
     private Vector3 fitness(){
         Vector3 coordinates = new Vector3();
-        Vector3 goal = terrain.getGoal();
+        Vector3  goal = this.goal.getLocation();
         coordinates.x = goal.x - ball.getX();
         coordinates.y = goal.y - ball.getY();
         return coordinates;
     }
+
+
 
 }
