a577ec988a626bd8ed9ad936dde4b330f06f3ddd
Juien ELIA <eliajulien@hotmail.com>
2018-05-29 13:51:26 +0200

    Comments

--- core/src/com/golf2k18/states/menu/EditorMenu.java
+++ core/src/com/golf2k18/states/menu/EditorMenu.java
@@ -17,6 +17,10 @@ public class EditorMenu extends SubMenu {
     private final String TITLE = "EDITOR MENU";
     private Table content;
 
+    /**
+     * Constructor for the EditorMenu class.
+     * @param manager object of the GameStateManager which is currently used.
+     */
     EditorMenu(StateManager manager) {
         super(manager);
         content = createContent();
@@ -29,6 +33,7 @@ public class EditorMenu extends SubMenu {
 
     private Table createContent() {
         Table table = new Table();
+        //"Create terrain from function" button
         TextButton fnctTerrain = new TextButton("Create terrain from function",StateManager.skin);
         fnctTerrain.addListener(new ClickListener() {
             @Override
@@ -40,6 +45,7 @@ public class EditorMenu extends SubMenu {
         table.add(fnctTerrain).pad(10f).fillX().expandX();
         table.row();
 
+        //"Create custom terrain" button
         TextButton splineTerrain = new TextButton("Create custom terrain",StateManager.skin);
         splineTerrain.addListener(new ClickListener() {
             @Override
@@ -52,7 +58,7 @@ public class EditorMenu extends SubMenu {
         });
         table.add(splineTerrain).pad(10f).fillX();
         table.row();
-
+        //"Create terrain" button
         TextButton courseCreation = new TextButton("Create terrain",StateManager.skin);
         courseCreation.addListener(new ClickListener() {
             @Override
