34083406079dde3b42654769d880d800f662f177
Andreas <andreaszurhaar@gmail.com>
2018-05-30 12:15:17 +0200

    AI implementation in game

--- core/src/com/golf2k18/ai/AI2.java
+++ core/src/com/golf2k18/ai/AI2.java
@@ -7,6 +7,7 @@ import com.golf2k18.handlers.Bot;
 import com.golf2k18.objects.Ball;
 import com.golf2k18.objects.Terrain;
 import com.golf2k18.states.game.Game;
+import com.golf2k18.states.menu.endStates.WinState;
 
 /**
  * The 2nd ai that can play the game. It tries to find the hole while always hitting the ball from the starting position.
@@ -14,9 +15,10 @@ import com.golf2k18.states.game.Game;
 public class AI2 extends Bot {
 
     private static Vector3 addVectors;
-        public AI2() {
+/*
+    public AI2() {
         addVectors = new Vector3();
-     }
+    }
 
     /*
     public Vector3 holeInOne(Game game) {
@@ -39,20 +41,31 @@ public class AI2 extends Bot {
     @Override
     public void handleInput(Game game) {
         super.handleInput(game);
-        if(Gdx.input.isKeyPressed(Input.Keys.Q))
-        {
+        if(!game.isHit(game.getBall())) {
+            Terrain terrain = game.getTerrain();
+            Ball ball = game.getBall();
+            Vector3 tryBall = fitness(terrain, ball);
+            // addVectors = addVectors.add(tryBall);
+            ball.hit(tryBall);
+        }
+        else{
+            game.getStateManager().push(new WinState(game.getStateManager()));
+        }
+        /*
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
 
-            if(!game.isHit(game.getBall())){
-                    Terrain terrain = game.getTerrain();
-                    Ball ball =  game.getBall();
-                    Vector3 tryBall = fitness(terrain,ball);
-                   // addVectors = addVectors.add(tryBall);
-                    ball.hit(tryBall);
-
+            while (!game.isHit(game.getBall())) {
+                Terrain terrain = game.getTerrain();
+                Ball ball = game.getBall();
+                Vector3 tryBall = fitness(terrain, ball);
+                // addVectors = addVectors.add(tryBall);
+                ball.hit(tryBall);
+            }
+            if(game.isHit(game.getBall())){
+                game.getStateManager().push(new WinState(game.getStateManager()));
 
             }
-
-
         }
+        */
     }
 }
