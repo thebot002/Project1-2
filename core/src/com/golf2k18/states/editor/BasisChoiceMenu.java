fe35a6680f3c4d26dc303e9392bd342e17abd7aa
thebot002 <31898693+thebot002@users.noreply.github.com>
2018-06-25 00:01:54 +0200

    choice of basis for spline editor

--- core/src/com/golf2k18/states/editor/BasisChoiceMenu.java
+++ core/src/com/golf2k18/states/editor/BasisChoiceMenu.java
@@ -1,26 +1,58 @@
 package com.golf2k18.states.editor;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 import com.golf2k18.StateManager;
+import com.golf2k18.io.DataIO;
 import com.golf2k18.states.SubMenu;
 
 public class BasisChoiceMenu extends SubMenu {
+    private final String TITLE = "TERRAIN BASIS";
+    private Table content;
+
     public BasisChoiceMenu(StateManager manager) {
         super(manager);
     }
 
     @Override
     protected void createContent() {
+        content = new Table();
 
+        Array<String> terrains = DataIO.getTerrainNames();
+        ButtonGroup<TextButton> buttons = new ButtonGroup<>();
+
+        ClickListener choice = new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                String s = buttons.getChecked().getText().toString();
+                manager.push(new TerrainEditor(manager,DataIO.getTerrain(s)));
+            }
+        };
+
+        Table buttonTable = new Table();
+        for (String t: terrains) {
+            TextButton b = new TextButton(t,StateManager.skin);
+            b.addListener(choice);
+            buttons.add(b);
+            buttonTable.add(b).pad(10f).expandX().fillX();
+            buttonTable.row();
+        }
+
+        content.add(new ScrollPane(buttonTable)).expand().fillX();
     }
 
     @Override
     protected Table getContent() {
-        return null;
+        return content;
     }
 
     @Override
     protected String getTitle() {
-        return null;
+        return TITLE;
     }
 }
