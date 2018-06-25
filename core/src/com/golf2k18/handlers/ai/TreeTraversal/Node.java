535ad040c6e0bcc1274d2e014216d5b8167a9af6
lucsparidans <lucsnadiraps@gmail.com>
2018-06-25 15:55:17 +0200

    A* first version

--- core/src/com/golf2k18/handlers/ai/TreeTraversal/Node.java
+++ core/src/com/golf2k18/handlers/ai/TreeTraversal/Node.java
@@ -2,13 +2,41 @@ package com.golf2k18.handlers.ai.TreeTraversal;
 
 import com.badlogic.gdx.math.Vector3;
 
+import java.util.ArrayList;
+
 public class Node<E> {
+
     private Vector3 location;
-    public Node(Vector3 location){
+    private ArrayList<Node> cameFrom;
+    private ArrayList<Node> neighbours;
+
+    public Node(Vector3 location, ArrayList<Node> cameFrom){
         this.location = location;
+        this.cameFrom = cameFrom;
 
     }
+
     public float distanceTo(Node to){
         return this.location.dst(to.location);
+    }
+
+    public void connectTo(Node neighbour){
+        this.neighbours.add(neighbour);
+    }
+
+    public ArrayList getNeighbours(){
+        return this.neighbours;
+    }
+
+    public Vector3 getLocation() {
+        return location;
+    }
+
+    public void addPrevious(Node n){
+        cameFrom.add(n);
+    }
+
+    public ArrayList<Node> getCameFrom() {
+        return cameFrom;
     }
 }
