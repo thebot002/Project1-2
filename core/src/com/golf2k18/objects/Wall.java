b7a2113a8602bad8b2319e1d71b904c8bb6fbb1e
Andreas <andreaszurhaar@gmail.com>
2018-06-26 14:19:41 +0200

    Collisions started

--- core/src/com/golf2k18/objects/Wall.java
+++ core/src/com/golf2k18/objects/Wall.java
@@ -1,20 +1,30 @@
 package com.golf2k18.objects;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.golf2k18.states.game.Game;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 public class Wall implements Collider,Serializable {
-    private Vector3 p0, p1, p00, p10, p01, p11;
+    private Vector3 p00, p10, p01, p11;
 
-    private final float thickness = .2f;
+    private float thickness = .2f;
+
+    private ModelInstance instance;
+    private static Texture brick = new Texture("Textures/brick_texture.jpg");
+    private int attr = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates;
+
 
     public Wall(Vector3 p0, Vector3 p1) {
-        this.p0 = p0;
-        this.p1 = p1;
-
         if(p0.equals(p1)){
             p00 = new Vector3(p0.x - thickness,p0.y - thickness,0);
             p01 = new Vector3(p0.x + thickness,p0.y - thickness,0);
@@ -27,22 +37,22 @@ public class Wall implements Collider,Serializable {
             p10 = new Vector3(p0.x - thickness,p1.y + thickness,0);
             p11 = new Vector3(p1.x + thickness,p1.y + thickness,0);
         }
+
+        Vector3 p0p1 = p1.cpy().sub(p0);
+        float angle = (float) Math.acos(p0p1.dot(1,0,0) / p0p1.len());
+
+        Quaternion orientation = new Quaternion(p0p1.cpy().crs(-1,0,0),(float)Math.toDegrees(angle));
+
+        Vector3 position = p0.cpy().add(p0p1.cpy().scl(.5f));
+
+        ModelBuilder builder = new ModelBuilder();
+        Model wall = builder.createBox(p0p1.len()+(2*thickness),2*thickness,2*thickness,
+                new Material(TextureAttribute.createDiffuse(brick)),attr);
+        instance = new ModelInstance(wall, new Matrix4(position,orientation,new Vector3(1,1,1)));
     }
 
-    @Override
-    public Wall collide(Game game) {
-        return game.getTerrain().getObstacles().get(0);
-    }
-
-    public ArrayList<Vector3> getCore(){
-        ArrayList<Vector3> points = new ArrayList<>();
-        points.add(p0);
-        points.add(p1);
-        return points;
-    }
-
-    public float getThickness(){
-        return thickness;
+    public ModelInstance getInstance() {
+        return instance;
     }
 
     @Override
@@ -50,8 +60,17 @@ public class Wall implements Collider,Serializable {
         return p01;
     }
 
+    public Vector3 getBottomLeftCorner(){return p00;}
+
     @Override
     public Vector3 getBottomRightCorner() {
         return p10;
     }
+
+    public Vector3 getTopRightCorner(){ return p11;}
+
+
+
+
+
 }
